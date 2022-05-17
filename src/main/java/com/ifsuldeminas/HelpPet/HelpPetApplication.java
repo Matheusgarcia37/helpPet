205463ab0115db73e1f4b4584bf05edd8032a529
Matheus Garcia Moreira <201911000041@IFSMP.PAS.IFSULDEMINAS.EDU.BR>
2022-05-17 11:32:23 -0300

    att

---
+++ src/main/java/com/ifsuldeminas/HelpPet/HelpPetApplication.java
@@ -0,0 +1,13 @@
+package com.ifsuldeminas.HelpPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifsuldeminas/HelpPet/HelpPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsuldeminas.HelpPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
